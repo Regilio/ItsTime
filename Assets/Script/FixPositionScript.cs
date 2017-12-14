537f8b7705f010ca19231434fae84969748dadee
PoniPono26 <34161013+PoniPono26@users.noreply.github.com>
2017-12-14 16:54:07 +0100

    a

---
+++ Assets/Script/FixPositionScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixPositionScript : MonoBehaviour
+{
+    
+
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            
+     
+            Debug.Log("Contact");
+          
+            other.transform.SetParent(transform);
+           
+
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+
+       
+            Debug.Log("sortie");
+          other.transform.SetParent(null);
+       
+
+        }
+    }
+
+
+}
---
+++ Assets/Script/ScriptRotative.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptRotative : MonoBehaviour {
+
+    Vector3 RotationA;
+    Vector3 RotationB;
+    float timer = 0;
+    float speed = 0.05f;
+
+
+    void Start()
+    {
+        RotationA = new Vector3(0, 0, 0);
+        RotationB = new Vector3(0, 360, 0);
+
+        StartCoroutine(NormalRotation());
+    }
+
+    void Update()
+    {
+  
+    }
+
+
+    IEnumerator NormalRotation()
+    {
+        while (true)
+        {
+            timer += Time.deltaTime * speed;
+            transform.localEulerAngles = Vector3.Lerp(RotationA, RotationB, timer);
+            if (timer > 1)
+                timer = 0;
+            yield return 0;
+        }
+
+    }
+}
