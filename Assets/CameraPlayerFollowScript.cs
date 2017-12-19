2061294349cc453f2c5d62a4f4c971ac6db2055e
Regilio <reggie_van_der_zel@hotmail.com>
2017-12-19 17:00:42 +0100

    Movement

---
+++ Assets/CameraPlayerFollowScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPlayerFollowScript : MonoBehaviour {
+
+	GameObject player;
+	public Vector3 pos;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+		pos = new Vector3 (player.transform.position.x - gameObject.transform.position.x, player.transform.position.y - gameObject.transform.position.y, player.transform.position.z - gameObject.transform.position.z);
+		transform.LookAt (player.transform);
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		gameObject.transform.position = player.transform.position - pos;
+	}
+}
